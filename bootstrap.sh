6113f92dcabeb362eab8850ef91518ef8754a518
dstarcev <starcev.da@gmail.com>
2016-01-30 23:58:52 +0300

    added git to vagrant

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+apt-get install -y git
 
 apt-get install -y --upgrade python python-pip python-dev \
   ipython-notebook python-numpy python-scipy python-pandas python-matplotlib
+
