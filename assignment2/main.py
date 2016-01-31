c719f41e1a6589f9682e57eee0b85ae2f5e86419
dstarcev <starcev.da@gmail.com>
2016-01-31 03:32:48 +0300

    assignment 2

---
+++ assignment2/main.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+
+with open("q1.txt", "w") as output:
+	data = pd.read_csv('titanic.csv', index_col='PassengerId')
+	data['Sex'] = pd.factorize(data['Sex'])[0]
+	complete = data[np.isnan(data.Age) == False]
+	X = complete[['Pclass', 'Fare', 'Age', 'Sex']]
+	y = complete['Survived']
+
+	clf = DecisionTreeClassifier(random_state = 241)
+	clf.fit(X, y)
+
+	result = pd.DataFrame({
+		'Name': X.columns,
+		'Weight': clf.feature_importances_}).sort('Weight', ascending=False).Name[:2].as_matrix()
+	output.write('%s %s' % tuple(result))
--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,5 @@ apt-get install -y git
 
 apt-get install -y --upgrade python python-pip python-dev \
   ipython-notebook python-numpy python-scipy python-pandas python-matplotlib
-
+  
+pip install -U sklearn
